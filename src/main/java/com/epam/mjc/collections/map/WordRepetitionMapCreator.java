5ccad4f7ef1ca8e85d6f187e7f5925f34c29c1f6
atainaive <atai.omorov@stud.vgtu.lt>
2022-11-27 13:39:01 +0200

    done

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,21 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> hashMap = new HashMap<>();
+
+        if (!sentence.isEmpty()) {
+            String key = sentence.toLowerCase(Locale.ROOT);
+            String[] words = key.split("[^a-z]+");
+            for (String s : words) {
+                hashMap.put(s, hashMap.getOrDefault(s, 0) + 1);
+            }
+        }
+
+        return hashMap;
     }
 }
